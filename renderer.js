1b04925765f2b188507fef725093560b9d56b536
Agustin_Mande <agustinlmande.am@gmail.com>
2025-07-15 20:24:58 -0300

    Primer commit

---
+++ renderer.js
@@ -0,0 +1,2 @@
+const information = document.getElementById('info')
+information.innerText = `Esta app usa Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), y Electron (v${versions.electron()})`
